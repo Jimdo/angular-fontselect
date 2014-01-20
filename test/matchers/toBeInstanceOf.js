178d423a7a732451a0c4c5a920266a4d9bee753a
Hannes Diercks <hannes.diercks@jimdo.com>
2014-01-20 17:02:51 +0100

    Fix additional matchers.

--- test/matchers/toBeInstanceOf.js
+++ test/matchers/toBeInstanceOf.js
@@ -1,9 +1,12 @@
-/* global jasmine */
-jasmine.Matchers.prototype.toBeInstanceOf = function(expectedInstance) {
-  var actual = this.actual;
-  var notText = this.isNot ? ' not' : '';
-  this.message = function() {
-    return 'Expected ' + actual.constructor.name + notText + ' is instance of ' + expectedInstance.name;
-  };
-  return actual instanceof expectedInstance;
-};
+beforeEach(function() {
+  this.addMatchers({
+    toBeInstanceOf: function(expectedInstance) {
+      var actual = this.actual;
+      var notText = this.isNot ? ' not' : '';
+      this.message = function() {
+        return 'Expected ' + actual.constructor.name + notText + ' is instance of ' + expectedInstance.name;
+      };
+      return actual instanceof expectedInstance;
+    }
+  });
+});
--- test/matchers/toBeTypeOf.js
+++ test/matchers/toBeTypeOf.js
@@ -1,14 +1,18 @@
-/* global jasmine */
-jasmine.Matchers.prototype.toBeTypeOf = function(expected) {
-  var actual, notText, objType;
- 
-  actual = this.actual;
-  notText = this.isNot ? 'not ' : '';
-  objType = actual ? Object.prototype.toString.call(actual) : '';
- 
-  this.message = function() {
-    return 'Expected ' + actual + notText + ' to be an array';
-  };
- 
-  return objType.toLowerCase() === '[object ' + expected.toLowerCase() + ']';
-};
+beforeEach(function() {
+  this.addMatchers({
+    toBeTypeOf: function(expected) {
+      var actual, notText, objType;
+
+      actual = this.actual;
+      notText = this.isNot ? 'not ' : '';
+      expected = expected.toLowerCase();
+      objType = typeof actual;
+
+      this.message = function() {
+        return 'Expected ' + actual + notText + ' to be type of ' + expected;
+      };
+     
+      return objType === expected;
+    }
+  });
+});
