b008483881d499ad17f33746e1bb2e1a251e9cac
Hannes Diercks <hannes.diercks@jimdo.com>
2014-01-20 11:54:29 +0100

    Add missing watch task.

---
+++ tasks/options/watch.js
@@ -0,0 +1,8 @@
+var files = require('../files');
+
+module.exports = {
+  andtest: {
+    files: files.source.concat([files.grunt, files.tests]),
+    tasks: ['ngtemplates', 'jshint', 'karma:watch:run']
+  }
+};
