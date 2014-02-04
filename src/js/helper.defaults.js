ee505f496f05bc034deedc5d080849aeab297a38
HerrBertling <markus277@gmail.com>
2014-02-04 10:28:39 +0100

    Added some websafe fonts, popularity not correct for now

--- src/js/helper.defaults.js
+++ src/js/helper.defaults.js
@@ -25,12 +25,76 @@ var DEFAULT_WEBSAFE_FONTS = [
     lastModified: '2014-01-28'
   },
   {
+    name: 'Consolas',
+    key: 'consolas',
+    category: 'other',
+    stack: 'Consolas, "Lucida Console", Monaco, monospace',
+    popularity: 1,
+    lastModified: '2014-02-04'
+  },
+  {
     name: 'Courier New',
     key: 'couriernew',
     category: 'other',
     stack: '"Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace',
     popularity: 1,
     lastModified: '2014-01-28'
+  },
+  {
+    name: 'Georgia',
+    key: 'georgia',
+    category: 'serif',
+    stack: 'Georgia, Palatino, "Palatino Linotype", Times, "Times New Roman", serif',
+    popularity: 2,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Helvetica',
+    key: 'helvetica',
+    category: 'sansserif',
+    stack: 'Helvetica, "Helvetica Neue", Arial, sans-serif',
+    popularity: 3,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Impact',
+    key: 'impact',
+    category: 'other',
+    stack: 'Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif',
+    popularity: 3,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Lucida Sans',
+    key: 'lucidasans',
+    category: 'sansserif',
+    stack: '"Lucida Sans", "Lucida Grande", "Lucida Sans Unicode", sans-serif',
+    popularity: 3,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Palatino',
+    key: 'palatino',
+    category: 'serif',
+    stack: 'Palatino, "Palatino Linotype", Georgia, Times, "Times New Roman", serif',
+    popularity: 2,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Tahoma',
+    key: 'tahoma',
+    category: 'sansserif',
+    stack: 'Tahoma, Verdana, Geneva, sans-serif',
+    popularity: 6,
+    lastModified: '2014-02-04'
+  },
+  {
+    name: 'Trebuchet',
+    key: 'trebuchet',
+    category: 'sansserif',
+    stack: '"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif',
+    popularity: 6,
+    lastModified: '2014-02-04'
   },
   {
     name: 'Verdana',
