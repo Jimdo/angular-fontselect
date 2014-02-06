9b769349ea7a2d3ea296c5b4dc273f6725352e2c
Hannes Diercks <hannes.diercks@jimdo.com>
2014-02-06 18:55:04 +0100

    Demo usage of selected font stack.

--- demo/app.js
+++ demo/app.js
@@ -2,8 +2,10 @@ angular.module('myApp', ['jdFontselect']).directive('myApp', function() {
   return {
     restrict: 'E',
     replace: false,
-    template: '<jd-fontselect />',
+    template: '<h1 style="font-family: {{selected.stack}}">Angular Fontselection, by Jimdo</h1>' +
+      '<jd-fontselect selected="selected"></jdFontselect>',
     controller: ['$scope', 'jdFontselect.fonts', function($scope, fontsService) {
+      $scope.selected = {};
       /* do something with $scope or the fontsService (for example, add a font) */
       fontsService.add({name: 'Foo', key: 'foo', stack: 'Foo, "Comic Sans", serif'});
     }]
