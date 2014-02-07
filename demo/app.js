190275cf6830f2e5a5f1f43c5b842fed0753e6c6
Hannes Diercks <hannes.diercks@jimdo.com>
2014-02-07 13:52:22 +0100

    Multiple selections in demo.

--- demo/app.js
+++ demo/app.js
@@ -1,14 +1,1 @@
-angular.module('myApp', ['jdFontselect']).directive('myApp', function() {
-  return {
-    restrict: 'E',
-    replace: false,
-    template: '<h1 style="font-family: {{selected.stack}}">Angular Fontselection, by Jimdo</h1>' +
-      '<jd-fontselect selected="selected"></jdFontselect>',
-    controller: ['$scope', 'jdFontselect.fonts', function($scope, fontsService) {
-      $scope.selected = {};
-      /* do something with $scope or the fontsService (for example, add a font) */
-      fontsService.add({name: 'Foo', key: 'foo', stack: 'Foo, "Comic Sans", serif'});
-    }]
-  };
-});
-angular.bootstrap(document, ['myApp']);
+angular.bootstrap(document, ['jdFontselect']);
