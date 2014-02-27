0544fca4d4d267663c1f5c27a6444a1b35addb47
Hannes Diercks <hannes.diercks@jimdo.com>
2014-02-27 18:14:04 +0100

    Use directive for default demo fonts.

--- demo/app.js
+++ demo/app.js
@@ -1,1 +1,6 @@
-angular.bootstrap(document, ['jdFontselect']);
+angular.module('jdFontselect.app', ['jdFontselect']).run(function($rootScope) {
+  $rootScope.header = '"Underdog", cursive, "google"';
+  $rootScope.content = '"Open Sans", sans-serif, "google"';
+  $rootScope.footer = '"Mate", serif, "google"';
+});
+angular.bootstrap(document, ['jdFontselect.app']);
