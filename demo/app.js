27fb998392180514d26d6d76f1e5fc1da267158b
Tina Umlandt <tina.umlandt@jimdo.com>
2016-02-18 19:01:46 +0100

    fix(Demo): show curated fonts in demo

--- demo/app.js
+++ demo/app.js
@@ -2,5 +2,7 @@ angular.module('jdFontselect.app', ['jdFontselect']).run(function($rootScope) {
   $rootScope.header = '"Underdog", fantasy, "google"';
   $rootScope.content = '"Open Sans", sans-serif, "google"';
   $rootScope.footer = '"Mate", serif, "google"';
-});
+}).config(["jdfsCuratedFontsProvider", function (jdfsCuratedFontsProvider) {
+  jdfsCuratedFontsProvider.setCuratedFontKeys(['websafe.timesnewroman', 'google.alef']);
+}])
 angular.bootstrap(document, ['jdFontselect.app']);
